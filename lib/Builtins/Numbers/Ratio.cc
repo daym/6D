62ff8e68730e69d9922678663e6585dc4436e82c
Danny Milosavljevic <dannym@scratchpost.org>
2013-01-22 13:27:01 +0000

    add Numbers

---
+++ lib/Builtins/Numbers/Ratio.cc
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdexcept>
+#include <assert.h>
+#include "Values/Values"
+#include "Numbers/Ratio"
+namespace Numbers {
+using namespace Values;
+Ratio* ratio(NodeT aa, NodeT bb) {
+	Ratio* result = new Ratio;
+	result->a = aa;
+	result->b = bb;
+	return(result);
+}
+bool ratio_P(NodeT n) {
+	return(dynamic_cast<const Ratio*>(n) != NULL);
+}
+/*
+static NodeT makeRatioB(NodeT options, NodeT argument) {
+	CXXArguments arguments = Evaluators::CXXfromArguments(options, argument);
+	CXXArguments::const_iterator iter = arguments.begin();
+	NodeT a = iter->second;
+	++iter;
+	NodeT b = iter->second;
+	//++iter;
+	return(ratio(a, b));
+} FIXME make ratio */
+static inline NodeT ensureRatio(NodeT node) {
+	if(!ratio_P(node))
+		throw std::invalid_argument("argument is not a Ratio");
+	return(node);
+}
+
+DEFINE_STRICT_FN(RatioP, (dynamic_cast<const Ratio*>(argument) !=NULL||dynamic_cast<const Ratio*>(argument) != NULL))
+DEFINE_STRICT_FN(RatioNumeratorGetter, Ratio_getA(ensureRatio(argument)))
+DEFINE_STRICT_FN(RatioDenominatorGetter, Ratio_getB(ensureRatio(argument)))
+
+/*
+a/b + c/d = (a*d + c*b)/(b*d)
+a/b - c/d = (a*d - c*b)/(b*d)
+a/b ⋅ c/d = a*c/(b*d)       
+a/b / c/d = a/b ⋅ d/c = a*d/(b*c)
+*/
+REGISTER_STR(Ratio, {
+	std::stringstream result;
+	result << '(' << Evaluators::str(node->a) << "/" << Evaluators::str(node->b) << ')';
+	return(result.str());
+})
+REGISTER_BUILTIN(RatioMaker, 2, 0, symbolFromStr("makeRatio"))
+REGISTER_BUILTIN(RatioP, 1, 0, symbolFromStr("ratio?"))
+REGISTER_BUILTIN(RatioNumeratorGetter, 1, 0, symbolFromStr("ratioNum"))
+REGISTER_BUILTIN(RatioDenominatorGetter, 1, 0, symbolFromStr("ratioDenom"))
+}; /* end namespace Numbers */
---
+++ lib/Builtins/Numbers/Real.cc
@@ -0,0 +1,93 @@
+#include <string.h>
+#include <limits>
+#include "Numbers/Real"
+#include "6D/FFIs"
+#include "Numbers/Integer"
+#include "Values/Values"
+
+namespace FFIs {
+Values::NodeT internNative(Numbers::NativeFloat value) {
+	return(new Numbers::Float(value));
+}
+}
+
+namespace Numbers {
+using namespace FFIs;
+using namespace Values;
+REGISTER_STR(Float, {
+	std::stringstream sst;
+	sst.precision(std::numeric_limits<NativeFloat>::digits10 + 1);
+	sst << node->value;
+	std::string v = sst.str();
+	const char* vc = v.c_str();
+	if(*vc == '-')
+		++vc;
+	if(strpbrk(vc, ".eE") == NULL && (vc[0] >= '0' && vc[0] <= '9')) { /* not a special value like inf or nan */
+		sst << ".0";
+		v = sst.str();
+	}
+	return(v);
+})
+
+REGISTER_STR(Real, return("FIXME");)
+
+
+bool floatP(NodeT node) {
+	return(dynamic_cast<const Float*>(node) != NULL);
+}
+DEFINE_STRICT_FN(FloatP, floatP(argument))
+
+NodeT operator+(const Float& a, const Float& b) {
+	return(internNative(a.value + b.value)); /* FIXME */
+}
+NodeT operator-(const Float& a, const Float& b) {
+	return(internNative(a.value - b.value)); /* FIXME */
+}
+NodeT operator*(const Float& a, const Float& b) {
+	return(internNative(a.value*b.value)); /* FIXME */
+}
+NodeT operator/(const Float& a, const Float& b) {
+	return(internNative(a.value/b.value)); /* FIXME */
+}
+NodeT operator<=(const Float& a, const Float& b) {
+	return(internNative(a.value <= b.value));
+}
+
+REGISTER_BUILTIN(FloatP, 1, 0, symbolFromStr("float?"))
+
+bool toNativeFloat(NodeT node, NativeFloat& result) {
+	const Float* floatNode;
+	result = 0.0;
+	//node = evaluate(node);
+	if(node == NULL)
+		return(false);
+	else if((floatNode = dynamic_cast<const Float*>(node)) != NULL) {
+		result = floatNode->value;
+		return(true);
+	} else {
+		// only coerce integers to float if there is no information loss
+		NativeInt value = 0;
+		if(!toNativeInt(node, value))
+			return(false);
+		result = (NativeFloat) value;
+		return((NativeInt) result == value);
+	}
+}
+
+#ifdef __GNUC__
+#define LLVM_INF           __builtin_inff()
+static Float nanFloat(__builtin_nanf(""));
+static Float infinityFloat(__builtin_inff());
+#else
+static Float nanFloat(0.0/0.0);
+static Float infinityFloat(1.0/0.0);
+#endif
+
+NodeT nan(void) {
+	return(&nanFloat);
+}
+NodeT infinity(void) {
+	return(&infinityFloat);
+}
+
+}; /* end namespace Numbers */
