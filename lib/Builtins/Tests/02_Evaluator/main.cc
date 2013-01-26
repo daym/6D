debf89b3dd43c0849f24c8b1e2a7dbb5e2de1d9e
Danny Milosavljevic <dannym@scratchpost.org>
2013-01-26 01:36:36 +0000

    enable eval objects

--- lib/Builtins/Tests/02_Evaluator/main.cc
+++ lib/Builtins/Tests/02_Evaluator/main.cc
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "Scanners/Lang5D"
 #include "Formatters/TExpression"
+#include "Evaluators/Evaluators"
 int main(int argc, char* argv[]) {
 	Scanners::Lang5D lang5D;
 	Values::NodeT prog = argc > 1 ? lang5D.parse1(fopen(argv[1], "r"), argv[1]) : lang5D.parse1(stdin, "<stdin>");
+	prog = Evaluators::eval(NULL, prog);
 	Formatters::TExpression::print(stdout, prog);
 	fflush(stdout);
 	return 0;
