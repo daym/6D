e2710e03f77ca1b6a4a1fa69113663ee0703ae80
Danny Milosavljevic <dannym@scratchpost.org>
2013-01-29 19:17:46 +0000

    add memoize to Builtins

--- lib/Builtins/Builtins.cc
+++ lib/Builtins/Builtins.cc
@@ -3,19 +3,23 @@
 #include "6D/Values"
 #include "6D/FFIs"
 #include "SpecialForms/SpecialForms"
+#include "Modulesystem/Memoizer"
 namespace Builtins {
 using namespace Values;
 using namespace FFIs;
 static NodeT SConstanter;
 static NodeT Srem;
 static NodeT Sexports;
+static NodeT Smemoize;
 static NodeT builtin(NodeT node) {
 	if(node == SConstanter)
 		return SpecialForms::Constanter;
 	else if(node == Srem)
 		return SpecialForms::Identer;
+	else if(node == Smemoize)
+		return Modulesystem::Memoizer;
 	else if(node == Sexports)
-		return cons(SConstanter, cons(Srem, nil));
+		return cons(SConstanter, cons(Srem, cons(Smemoize, nil)));
 	else
 		return nil; // FIXME error
 }
