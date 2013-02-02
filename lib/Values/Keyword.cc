f684f22d08bd6763e44dbeb9722cd3dcb66af816
Danny Milosavljevic <dannym@scratchpost.org>
2013-02-02 11:33:06 +0000

    make keywordP work

--- lib/Values/Keyword.cc
+++ lib/Values/Keyword.cc
@@ -6,7 +6,7 @@
 namespace Values {
 
 bool keywordP(NodeT node) {
-	return tagOfNode(node) == TAG_SYMBOL;
+	return tagOfNode(node) == TAG_KEYWORD;
 }
 struct hashstr2 {
 	unsigned long operator()(const char* str) const {
