5a8b73cb617b05f9226f4a641b2f31a00b68d966
Danny Milosavljevic <dannym@scratchpost.org>
2013-02-03 22:29:40 +0000

    add equal, lessequal

--- lib/Numbers/Arithmetic.c
+++ lib/Numbers/Arithmetic.c
@@ -9,6 +9,9 @@ static NodeT Sstar;
 static NodeT Sslash;
 static NodeT Sshl;
 static NodeT Sshr;
+static NodeT Slessequal;
+static NodeT Sequal;
+static NodeT int0;
 /* TODO promotion */
 static NodeT addA(NodeT a, NodeT b) {
 	return integerAdd(a, b);
@@ -20,8 +23,7 @@ static NodeT subtractA(NodeT a, NodeT b) {
 }
 DEFINE_STRICT_BINARY_FN(subtract, subtractA(env, argument))
 static NodeT multiplyA(NodeT a, NodeT b) {
-	printf("MULTIPLYING\n");
-	return a;
+	return integerMul(a, b);
 }
 DEFINE_STRICT_BINARY_FN(multiply, multiplyA(env, argument))
 static NodeT divideA(NodeT a, NodeT b) {
@@ -44,6 +46,15 @@ static NodeT shrA(NodeT amount, NodeT b) {
 	return integerShr(b, value);
 }
 DEFINE_STRICT_BINARY_FN(shr, shrA(env, argument))
+static NodeT lePA(NodeT a, NodeT b) {
+	return subtractA(a, b); /* TODO <= 0 */
+}
+DEFINE_STRICT_BINARY_FN(leP, lePA(env, argument))
+static NodeT equalPA(NodeT a, NodeT b) {
+	/* TODO make this less brittle */
+	return internNativeBool(a == b || subtractA(a, b) == int0);
+}
+DEFINE_STRICT_BINARY_FN(equalP, equalPA(env, argument))
 void initArithmetic(void) {
 	Splus = symbolFromStr("+");
 	Sdash = symbolFromStr("-");
@@ -51,14 +62,19 @@ void initArithmetic(void) {
 	Sslash = symbolFromStr("/");
 	Sshl = symbolFromStr("shl");
 	Sshr = symbolFromStr("shr");
+	Slessequal = symbolFromStr("<=");
+	Sequal = symbolFromStr("=");
 	initIntegers();
 	initFloats();
+	int0 = internNativeUInt(0);
 	INIT_BINARY_FN(add)
 	INIT_BINARY_FN(subtract)
 	INIT_BINARY_FN(multiply)
 	INIT_BINARY_FN(divide)
 	INIT_BINARY_FN(shl)
 	INIT_BINARY_FN(shr)
+	INIT_BINARY_FN(equalP)
+	INIT_BINARY_FN(leP)
 }
 NODET withArithmetic(NODET body) {
 	return closeOver(Splus, add, 
@@ -67,6 +83,8 @@ NODET withArithmetic(NODET body) {
 	       closeOver(Sslash, divide,
 	       closeOver(Sshl, shl,
 	       closeOver(Sshr, shr,
-	       body))))));
+	       closeOver(Sequal, equalP,
+	       closeOver(Slessequal, leP,
+	       body))))))));
 }
 END_NAMESPACE_6D(Arithmetic)
