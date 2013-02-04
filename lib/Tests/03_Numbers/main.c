2289d82f1398815600e04fe7504ace2913fb956c
Danny Milosavljevic <dannym@scratchpost.org>
2013-02-04 08:42:09 +0000

    add arithmetic tests

--- lib/Tests/03_Numbers/main.c
+++ lib/Tests/03_Numbers/main.c
@@ -4,6 +4,7 @@
 #include "6D/Values"
 #include "6D/Evaluators"
 #include "6D/Builtins"
+#include "6D/Arithmetic"
 USE_NAMESPACE_6D(Values)
 USE_NAMESPACE_6D(Allocators)
 USE_NAMESPACE_6D(Evaluators)
@@ -11,17 +12,17 @@ USE_NAMESPACE_6D(Builtins)
 USE_NAMESPACE_6D(Formatters::TExpression)
 int main(int argc, char* argv[]) {
 	initAllocators();
-	initIntegers();
+	initArithmetic();
 	initEvaluator();
 	NodeT defaultDynEnv = initLang5D();
 	NodeT prog = argc > 1 ? L_parse1(fopen(argv[1], "r"), argv[1]) : L_parse1(stdin, "<stdin>");
 	// TODO Memoize
-	prog = close(symbolFromStr("Builtins"), initBuiltins(), prog);
-	print(stderr, prog);
-	fprintf(stderr, "\n");
+	prog = closeOver(symbolFromStr("Builtins"), initBuiltins(), withArithmetic(L_withDefaultEnv(prog)));
+	//print(stderr, prog);
+	//fprintf(stderr, "\n");
 	prog = annotate(defaultDynEnv, prog); //lang5D.withDefaultEnv(prog));
-	print(stderr, prog);
-	fprintf(stderr, "\n");
+	//print(stderr, prog);
+	//fprintf(stderr, "\n");
 	prog = eval(prog);
 	print(stdout, prog);
 	fflush(stdout);
