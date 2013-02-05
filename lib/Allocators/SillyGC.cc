03028f0eb9b124b9186e71b4e0db20689069ec31
Danny Milosavljevic <dannym@scratchpost.org>
2013-02-05 19:51:49 +0000

    fix SillyGC

---
+++ lib/Allocators/SillyGC.cc
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#ifdef __cplusplus
+#include <set>
+#endif
+#include "6D/Allocators"
+#include "6D/Values"
+#ifndef GC_THREADS
+BEGIN_NAMESPACE_6D(Allocators)
+static std::set<gc*>* arena;
+extern "C"
+void* GC_malloc(size_t size) {
+	void* result = malloc(size);
+	if(!arena)
+		arena = new std::set<void*>;
+	arena->insert(result);
+	return result;
+}
+extern "C"
+void* GC_malloc_atomic(size_t size) {
+	return GC_malloc(size);
+}
+extern "C"
+void* GC_malloc_uncollectable(size_t size) {
+	void* result = malloc(size);
+	return result;
+}
+extern "C"
+void zap(void) {
+	if(arena)
+	for(std::set<void*>::const_iterator iter = arena->begin(); iter != arena->end(); ++iter) {
+		void* p = *iter;
+		free(p);
+	}
+}
+END_NAMESPACE_6D(Allocators)
+#else
+extern "C"
+void zap(void) {
+}
+#endif
