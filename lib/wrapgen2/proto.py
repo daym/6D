f5ed202293a9f094f72dec47e34f76ab461922f4
Danny Milosavljevic <dannym@scratchpost.org>
2013-02-06 10:26:02 +0000

    port to libxml2

--- lib/wrapgen2/proto.py
+++ lib/wrapgen2/proto.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
 import sys
-from xml.dom import minidom
+import libxml2
 def childElements(pnode):
-	for node in pnode.childNodes:
-		if node.nodeType != node.ELEMENT_NODE:
+	for node in pnode.children or []:
+		if node.type != "element":
 			continue
 		yield node
+elementCacheById = {}
 def elementById(dom, id):
-	for element in childElements(dom.documentElement):
-		if element.getAttribute("id") == id:
-			return element
-	return None
+	result = elementCacheById.get(id)
+	if result is not None:
+		return result
+	result = None
+	for element in childElements(dom):
+		if element.prop("id") == id:
+			result = element
+			break
+	elementCacheById[id] = result
+	return result
 def members(dom, pnode):
-	ids = pnode.getAttribute("members")
+	ids = pnode.prop("members")
+	if ids is None:
+		return
 	ids = ids.strip()
 	if ids == "":
 		return
@@ -25,13 +34,13 @@ def members(dom, pnode):
 			print >>sys.stderr, "warning: did not find element with id %r" % (id, )
 baseTypesById = {}
 def fundamentalTypeP(element):
-	return element.tagName == "FundamentalType"
+	return element.name == "FundamentalType"
 def calcBaseTypeById(dom, id):
 	element = elementById(dom, id)
 	if fundamentalTypeP(element):
-		return element.getAttribute("name")
+		return element.prop("name")
 	else:
-		print element.tagName, "unknown"
+		print element.name, "unknown"
 		assert(False)
 		
 def baseTypeById(dom, id):
@@ -46,33 +55,32 @@ def baseTypeById(dom, id):
 	else:
 		print >>sys.stderr, "warning: ignored empty type id"
 def namespaceP(node):
-	return node.tagName == "Namespace"
+	return node.name == "Namespace"
 def classP(node):
-	return node.tagName == "Class"
+	return node.name == "Class"
 def structP(node):
-	return node.tagName == "Struct"
+	return node.name == "Struct"
 def functionP(node):
-	return node.tagName == "Function"
+	return node.name == "Function"
 def methodP(node): 
-	return node.tagName == "Method"
+	return node.name == "Method"
 def argumentP(node):
-	return node.tagName == "Argument"
+	return node.name == "Argument"
 # TODO constructor, destructor
 def processArgument(dom, indentation, pnode):
-	print "%*s%s : %s" % (indentation*2, "", pnode.getAttribute("name"), baseTypeById(dom, pnode.getAttribute("type")))
+	print "%*s%s : %s" % (indentation*2, "", pnode.prop("name"), baseTypeById(dom, pnode.prop("type")))
 def processFunction(dom, indentation, pnode):
-	print "%*s%s" % (indentation*2, "", pnode.getAttribute("name"))
+	print "%*s%s" % (indentation*2, "", pnode.prop("name"))
 	for argument in childElements(pnode):
-		if argument.tagName != "Argument":
-			continue
-		processArgument(dom, indentation + 1, pnode)
+		if argumentP(argument):
+			processArgument(dom, indentation + 1, pnode)
 def processMethod(dom, indentation, pnode):
-	print "%*s%s" % (indentation*2, "", pnode.getAttribute("name"))
+	print "%*s%s" % (indentation*2, "", pnode.prop("name"))
 	for element in childElements(pnode):
 		if argumentP(element):
 			processArgument(dom, indentation + 1, element)
 def processNamespace(dom, indentation, pnode): # or class
-	print "%*s%s" % (indentation*2, "", pnode.getAttribute("name"))
+	print "%*s%s" % (indentation*2, "", pnode.prop("name"))
 	for element in members(dom, pnode):
 		if namespaceP(element):
 			processNamespace(dom, indentation + 1, element)
@@ -85,8 +93,8 @@ def processNamespace(dom, indentation, pnode): # or class
 		elif methodP(element):
 			processMethod(dom, indentation + 1, element)
 def parse(name):
-	dom = minidom.parse(name)
-	rootNamespace = [node for node in childElements(dom.documentElement) if namespaceP(node) and node.getAttribute("name") == "::"][0]
+	dom = libxml2.parseFile(name)
+	rootNamespace = [node for node in childElements(dom.children) if namespaceP(node) and node.prop("name") == "::"][0]
 	processNamespace(dom, 0, rootNamespace)
 
 if __name__ == "__main__":
