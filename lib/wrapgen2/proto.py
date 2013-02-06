475a0e492281090ac5c06463ec8f159b0cbd2500
Danny Milosavljevic <dannym@scratchpost.org>
2013-02-06 10:16:33 +0000

    extend wrapgen2

--- lib/wrapgen2/proto.py
+++ lib/wrapgen2/proto.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys
 from xml.dom import minidom
 def childElements(pnode):
 	for node in pnode.childNodes:
@@ -22,6 +23,28 @@ def members(dom, pnode):
 			yield result
 		else:
 			print >>sys.stderr, "warning: did not find element with id %r" % (id, )
+baseTypesById = {}
+def fundamentalTypeP(element):
+	return element.tagName == "FundamentalType"
+def calcBaseTypeById(dom, id):
+	element = elementById(dom, id)
+	if fundamentalTypeP(element):
+		return element.getAttribute("name")
+	else:
+		print element.tagName, "unknown"
+		assert(False)
+		
+def baseTypeById(dom, id):
+	global baseTypesById
+	if id:
+		result = baseTypesById.get(id)
+		if result:
+			return result
+		result = calcBaseTypeById(dom, id)
+		baseTypesById[id] = result
+		return result
+	else:
+		print >>sys.stderr, "warning: ignored empty type id"
 def namespaceP(node):
 	return node.tagName == "Namespace"
 def classP(node):
@@ -32,9 +55,11 @@ def functionP(node):
 	return node.tagName == "Function"
 def methodP(node): 
 	return node.tagName == "Method"
+def argumentP(node):
+	return node.tagName == "Argument"
 # TODO constructor, destructor
 def processArgument(dom, indentation, pnode):
-	print "%*s%s" % (indentation*2, "", pnode.getAttribute("name"))
+	print "%*s%s : %s" % (indentation*2, "", pnode.getAttribute("name"), baseTypeById(dom, pnode.getAttribute("type")))
 def processFunction(dom, indentation, pnode):
 	print "%*s%s" % (indentation*2, "", pnode.getAttribute("name"))
 	for argument in childElements(pnode):
@@ -43,6 +68,9 @@ def processFunction(dom, indentation, pnode):
 		processArgument(dom, indentation + 1, pnode)
 def processMethod(dom, indentation, pnode):
 	print "%*s%s" % (indentation*2, "", pnode.getAttribute("name"))
+	for element in childElements(pnode):
+		if argumentP(element):
+			processArgument(dom, indentation + 1, element)
 def processNamespace(dom, indentation, pnode): # or class
 	print "%*s%s" % (indentation*2, "", pnode.getAttribute("name"))
 	for element in members(dom, pnode):
@@ -54,10 +82,12 @@ def processNamespace(dom, indentation, pnode): # or class
 			processNamespace(dom, indentation + 1, element)
 		elif functionP(element):
 			processFunction(dom, indentation + 1, element)
+		elif methodP(element):
+			processMethod(dom, indentation + 1, element)
 def parse(name):
 	dom = minidom.parse(name)
 	rootNamespace = [node for node in childElements(dom.documentElement) if namespaceP(node) and node.getAttribute("name") == "::"][0]
 	processNamespace(dom, 0, rootNamespace)
 
 if __name__ == "__main__":
-	parse("test.xml")
+	parse(sys.argv[-1] if len(sys.argv) > 1 else "test.xml")
