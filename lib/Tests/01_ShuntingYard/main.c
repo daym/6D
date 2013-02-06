0af5c977cb6f0609af837397bb4480ac0a4cafc7
Danny Milosavljevic <dannym@scratchpost.org>
2013-02-06 21:44:37 +0000

    fix test program

--- lib/Tests/01_ShuntingYard/main.c
+++ lib/Tests/01_ShuntingYard/main.c
@@ -2,17 +2,17 @@
 #include "6D/Values"
 #include "6D/Allocators"
 #include "Parsers/Lang5D"
-#include "Formatters/TExpression"
 USE_NAMESPACE_6D(Values)
 USE_NAMESPACE_6D(Allocators)
 USE_NAMESPACE_6D(Formatters::TExpression)
 USE_NAMESPACE_6D(Parsers)
+#include "../sillyprint.inc"
 int main(int argc, char* argv[]) {
 	initAllocators();
 	initIntegers();
-	initLang5D();
-	NodeT prog = argc > 1 ? L_parse1(fopen(argv[1], "r"), argv[1]) : L_parse1(stdin, "<stdin>");
-	print(stdout, prog);
+	initLang();
+	NodeT prog = argc > 1 ? Lang_parse1(fopen(argv[1], "r"), argv[1]) : Lang_parse1(stdin, "<stdin>");
+	S_print(prog);
 	fflush(stdout);
 	return 0;
 }
